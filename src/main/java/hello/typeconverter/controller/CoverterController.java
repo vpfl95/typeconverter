370a7fbe8b434d447af0dcc3e518cc523c15abee
vpfl95 <k_dh610@naver.com>
2023-04-02 21:24:23 +0900

    뷰 템플릿에 컨버터 적용하기

---
+++ src/main/java/hello/typeconverter/controller/CoverterController.java
@@ -0,0 +1,46 @@
+package hello.typeconverter.controller;
+
+import hello.typeconverter.type.IpPort;
+import lombok.Data;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CoverterController {
+
+    @GetMapping("/converter-view")
+    public String converterView(Model model){
+        model.addAttribute("number",10000);
+        model.addAttribute("ipPort",new IpPort("127.0.0.1",8080));
+        return "converter-view";
+    }
+
+    //자동으로 컨버터적용
+    @GetMapping("/converter/edit")
+    public String converterForm(Model model){
+        IpPort ipPort = new IpPort("127.0.0.1", 8080);
+        Form form = new Form(ipPort);
+        model.addAttribute("form",form);
+        return "converter-form";
+    }
+
+    @PostMapping("/converter/edit")
+    public String converterEdit(@ModelAttribute Form form, Model model){
+        IpPort ipPort = form.getIpPort();
+        model.addAttribute("ipPort",ipPort);
+        return "converter-view";
+    }
+
+    @Data
+    static class Form{
+        private IpPort ipPort;
+
+        public Form(IpPort ipPort) {
+            this.ipPort = ipPort;
+        }
+    }
+
+}
